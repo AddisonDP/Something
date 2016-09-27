942824393439fb127d7ca35aef15b7dd92041bf9
AddisonDP <addisonpickrell@gmail.com>
2016-09-26 19:59:45 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,3 +5,18 @@ function gimmegooglefont(name){
   el.rel = "stylesheet";
   document.body.appendchild(el);
 }
+function calculator(funct, num1, num2){
+  switch(funct):
+    case "add":
+      return num1 + num2;
+      break;
+    case "sub":
+      return num1 - num2;
+      break;
+    case "mul":
+      return num1 * num2;
+      break;
+    case "div":
+      return num1 / num2;
+      break;
+}
